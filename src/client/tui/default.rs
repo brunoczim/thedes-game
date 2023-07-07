6a007a93f679a53c1a5c997ad3bc2fba7f4bba7a
brunoczim <brunoczim@gmail.com>
2023-07-06 22:23:39 -0300

    stub

---
+++ src/client/tui/default.rs
@@ -0,0 +1,6 @@
+use crate::domain::client::Client as ClientTrait;
+
+#[derive(Debug)]
+pub struct Client {}
+
+impl ClientTrait for Client {}
---
+++ src/server/default.rs
@@ -0,0 +1,6 @@
+use crate::domain::server::Server as ServerTrait;
+
+#[derive(Debug)]
+pub struct Server {}
+
+impl ServerTrait for Server {}

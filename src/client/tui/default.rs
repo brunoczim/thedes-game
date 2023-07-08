f6c384cf9351c78678358c0869d1fcf44c1c26f5
brunoczim <brunoczim@gmail.com>
2023-07-08 18:22:06 -0300

    advancing tui client

--- src/client/tui/default.rs
+++ src/client/tui/default.rs
@@ -1,13 +1,54 @@
-use andiskaz::terminal::Terminal;
+use std::collections::HashMap;
 
-use crate::domain::{client::Client as ClientTrait, error::Result};
+use andiskaz::{coord::Coord, event::Event, terminal::Terminal};
+use gardiz::{coord::Vec2, map::Map};
+
+use crate::domain::{
+    client::Client as ClientTrait,
+    entity::{npc::Npc, player::Player},
+    error::Result,
+    id,
+    map::Cell,
+    server::Server as ServerTrait,
+    thede::Thede,
+};
+
+#[derive(Debug, Clone)]
+struct Game {
+    npcs: HashMap<id::Npc, Npc>,
+    players: HashMap<id::Player, Player>,
+    thedes: HashMap<id::Thede, Thede>,
+    player: Player,
+    map: Map<Coord, Cell>,
+}
+
+impl Game {
+    fn new(player: Player) -> Self {
+        Self {
+            npcs: HashMap::new(),
+            players: HashMap::from([(player.id, player.clone())]),
+            thedes: HashMap::new(),
+            player,
+            map: Map::new(),
+        }
+    }
+}
 
 #[derive(Debug)]
-pub struct Client {}
+pub struct Client<S>
+where
+    S: ServerTrait + Send + 'static,
+{
+    server: S,
+    game: Game,
+}
 
-impl Client {
-    pub fn new() -> Self {
-        Self {}
+impl<S> Client<S>
+where
+    S: ServerTrait + Send + 'static,
+{
+    pub fn new(player: Player, server: S) -> Self {
+        Self { server, game: Game::new(player) }
     }
 
     pub async fn run(self) -> Result<()> {
@@ -15,9 +56,18 @@ impl Client {
         Ok(())
     }
 
-    async fn main(self, term: Terminal) -> Result<()> {
+    async fn main(self, mut term: Terminal) -> Result<()> {
+        loop {
+            let mut guard = term.listen().await?;
+            if let Some(event) = guard.event() {
+                match event {
+                    Event::Resize(resize_evt) => {},
+                    Event::Key(key_evt) => {},
+                }
+            }
+        }
         Ok(())
     }
 }
 
-impl ClientTrait for Client {}
+impl<S> ClientTrait for Client<S> where S: ServerTrait + Send + 'static {}
--- src/domain/map.rs
+++ src/domain/map.rs
@@ -2,8 +2,6 @@ use super::{biome::Biome, block::Block, ground::Ground, id};
 
 pub type Coord = u16;
 
-pub const CHUNK_SIZE: usize = 5;
-
 #[derive(
     Debug,
     Clone,
@@ -21,19 +19,4 @@ pub struct Cell {
     pub ground: Ground,
     pub block: Option<Block>,
     pub thede: Option<id::Thede>,
-}
-
-#[derive(
-    Debug,
-    Clone,
-    PartialEq,
-    Eq,
-    PartialOrd,
-    Ord,
-    Hash,
-    serde::Serialize,
-    serde::Deserialize,
-)]
-pub struct Chunk {
-    pub cells: Box<[[Cell; CHUNK_SIZE]; CHUNK_SIZE]>,
 }
